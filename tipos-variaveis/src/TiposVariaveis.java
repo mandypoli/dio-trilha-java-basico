071dc00677cb847639bdfc1ec2fa6d1864b79c06
Amanda Pereira <amandiholiv@gmail.com>
2025-04-18 10:06:08 -0300

    atualizacao curso

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
     }
 }
